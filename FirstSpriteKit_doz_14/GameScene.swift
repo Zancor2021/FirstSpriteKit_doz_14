388a353b7b3bf8b99909b0573145e2407e8b01bc
Alex <alex.hoch@gmx.de>
2021-01-26 13:14:01 +0100

    Physik and push

--- FirstSpriteKit_doz_14/GameScene.swift
+++ FirstSpriteKit_doz_14/GameScene.swift
@@ -9,64 +9,78 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
+    /************START****************/
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+         setPhysicWorld()
+    }
+    /*****************************************/
+    func createCircle(){
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
     }
     
+    func createRect(){
+        
+    }
+    
+    func createSpriteWithImage(pos: CGPoint){
+        let pig:SKSpriteNode = SKSpriteNode(imageNamed: "p1")
+               pig.position.x = startPos.x
+               pig.position.y = startPos.y
+               pig.size = CGSize(width: 100,height: 100)
+        
+               pig.physicsBody = SKPhysicsBody(circleOfRadius: (pig.frame.height/2)-5)
+               pig.physicsBody?.allowsRotation = true
+               pig.physicsBody?.mass =  5
+               pig.physicsBody?.friction = 100
+        
+          
+           self.addChild(pig)
+         setPush(obj: pig, dx: 100, dy: -100)
+    }
+    
+    func createParticle(){
+        
+    }
+    /************************************/
+    func setPush(obj:SKSpriteNode,dx:CGFloat,dy:CGFloat){
+         let dxnew = startPos.x - dx
+         let dynew = startPos.y - dy
+            (obj.physicsBody?.applyForce(CGVector(dx: dxnew*3000, dy: dynew*3000)))!
+       }
+    /*****************************************/
+    func setPhysicWorld(){
+              //erzeuge ein Rechteck mit Physic das so gross ist wie diese GameScene
+              let rect:CGRect = CGRect(x: 0,y: 0,width: self.frame.width,height: self.frame.height)
+              self.physicsBody = SKPhysicsBody(edgeLoopFrom: rect)
+              
+              //setze die Gravitation in diesem Rechteck
+              self.physicsWorld.gravity = CGVector(dx: 0, dy:0)
+           //**2**.
+              self.physicsWorld.contactDelegate = self
+    }
+    
+    /************************/
+    var startPos = CGPoint(x: 500, y: 500)
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+       startPos.x = pos.x
+        startPos.y = pos.y
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+       
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+         createSpriteWithImage(pos:pos)
     }
     
+    /************************/
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
+               
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
@@ -86,4 +100,5 @@ class GameScene: SKScene {
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
+    /***********************/
 }
